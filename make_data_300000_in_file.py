f32abb2f14050f982a94ede4ca73a8eb76ad7693
STRAFFIC_KKM\straf <1588760@daum.net>
2023-12-15 16:25:41 +0900

    300000개 랜덤데이터 생성 및 파일 쓰기

---
+++ make_data_300000_in_file.py
@@ -0,0 +1,20 @@
+import random
+from datetime import datetime,timedelta
+
+def makefile():
+    group_name = random.randint(1000000,2000000)
+    name = random.randint(1000,9999)
+    score = random.randint(0,100)
+    input_date = datetime(2023,12,1) + timedelta(days=random.randint(0,30))
+    return f"{group_name},{name},{score},{input_date}\n"
+
+
+if __name__ == "__main__" :
+    file_path = 'E:\python-sample-github\\'
+    file_name = 'data_300000'
+
+    with open(file_path + file_name, 'w') as fd:
+        data_list = [makefile() for _ in range(300000)]
+        fd.writelines(data_list)
+
+
